55b51c7945e3c604b4cb21ae2f1de925b7023874
Andy Fragen <andy@thefragens.com>
2018-06-15 11:12:12 -0700

    oops

--- src/EDD_Translations_Updater/Init.php
+++ src/EDD_Translations_Updater/Init.php
@@ -29,8 +29,8 @@ class Init {
 	 * Let's get going. Load relevant action/filter hooks.
 	 */
 	public function run() {
-		add_action( 'post_EDD_plugin_updater_setup', array( &$this, 'get_edd_plugin_data' ), 15, 1 );
-		add_action( 'post_EDD_theme_updater_setup', array( &$this, 'get_edd_theme_data' ), 15, 1 );
+		add_action( 'post_edd_sl_plugin_updater_setup', array( &$this, 'get_edd_plugin_data' ), 15, 1 );
+		add_action( 'post_edd_sl_theme_updater_setup', array( &$this, 'get_edd_theme_data' ), 15, 1 );
 	}
 
 	/**
